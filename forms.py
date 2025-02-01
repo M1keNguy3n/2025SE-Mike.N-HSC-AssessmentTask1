ca2a5284618de24286ab82699b1f67e3e92f0a28
M1keNguy3n <mike.nguyen696@education.nsw.gov.au>
2025-02-01 09:28:42 +0700

    0.0.10
    Added a missing dependency

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, URLField, TextAreaField, SelectField, DateTimeField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, URL
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(message='Please enter a valid email address')])
